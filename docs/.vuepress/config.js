591ff8cad1dbe607e456b4b32db0dd038f8ab756
whistle <435424527@qq.com>
2025-02-17 11:06:10 +0800

    202502171106

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,53 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import { slimsearchPlugin } from '@vuepress/plugin-slimsearch'
+/* 
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Cache-Control" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />
+*/
+export default defineUserConfig({
+  bundler: viteBundler(),
+  title: "初中信息科技学科教学仪器-智能控制板 教程",
+  head: [
+    ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
+    ["meta", {"http-equiv": "Cache-Control", content: "no-cache"}],
+    ["meta", {"http-equiv": "Expires", content: "0"}]
+  ],
+  theme: defaultTheme({
+    home: "/getting-started/概要.md",
+    logo: "/images/cfdsxdoc-icon.png",
+    logoAlt: "[当前网络不佳]",
+    contributors: "Whistle Wang",
+
+    sidebar: [
+      {
+        text: "准备工作",
+        link: "/getting-started/快速索引.md",
+        children: [
+          {text: "快速索引", link: "/getting-started/快速索引.md"}
+        ]
+      },
+      {
+        text: " 物联网的数据采集",
+        link: "/_01_collection/声音数据的采集.md",
+        collapsible: true,
+        children: [
+          {text: "声音数据的采集", link: "/_01_lamp/声音数据的采集.md"},
+        ]
+      },
+    ],
+    sidebarDepth: 0
+  }),
+
+  base: "/tutorial/cfipyb/",
+
+  plugins: [
+    slimsearchPlugin({
+      indexContent: true,
+    }),
+    backToTopPlugin()
+  ],
+})
